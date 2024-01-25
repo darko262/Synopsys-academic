bec1ed0fa8e50f0fba941d6bc11d1f8bc234d477
darko262 <s.padilla01@ufromail.cl>
2024-01-25 18:43:39 -0300

    Ejercicios para academia

---
+++ TCL/primerosPasos.tcl
@@ -0,0 +1,15 @@
+# Este es un comentario en Tcl
+
+# Definir una variable
+set mensaje "Hola, mundo!"
+
+# Imprimir el mensaje en la consola
+puts $mensaje
+
+# Definir una lista
+set numeros {1 2 3 4 5}
+
+# Iterar sobre la lista y imprimir cada elemento
+foreach num $numeros {
+    puts "Número: $num"
+}
