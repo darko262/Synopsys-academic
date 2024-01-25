9dcf699ed78541c1d03e29ffc8212b1b1310def1
darko262 <s.padilla01@ufromail.cl>
2024-01-25 19:54:38 -0300

    actulizando tcl

--- TCL/primerosPasos.tcl
+++ TCL/primerosPasos.tcl
@@ -13,3 +13,23 @@ set numeros {1 2 3 4 5}
 foreach num $numeros {
     puts "Número: $num"
 }
+
+#matrices y arreglos
+set m1(uno) 123456
+puts $m1(uno)
+set m1(dos) 789101112
+puts $m1(dos)
+
+#arreglos no ocupar la letra ñ
+set tamano [array size m1 ]
+puts $tamano
+
+set nombres [array names m1]
+puts $nombres
+
+set matriz [array get m1]
+puts $matriz
+
+foreach {name contenido} [array get m1] {
+    puts "$name $contenido"
+}
